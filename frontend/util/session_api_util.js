c1f382836db800f237b38b6c0da29feeb820af05
Torben Ginsberg <tginsberg8@gmail.com>
2022-08-12 14:33:37 -0400

    session_api_util ajax requests

---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,24 @@
+// AJAX requests for backend routes
+
+export const signup = user => (
+    $.ajax({
+        url: 'api/user',
+        method: 'POST',
+        data: { user }
+    })
+);
+
+export const login = user => (
+    $.ajax({
+        url: 'api/session',
+        method: 'POST',
+        data: { user }
+    })
+);
+
+export const logout = () => (
+    $.ajax({
+        url: 'api/session',
+        method: 'DELETE'
+    })
+);
