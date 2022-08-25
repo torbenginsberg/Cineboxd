6b16f21a2ed76ef89dd8dbce735a606f1de68a8d
Torben Ginsberg <tginsberg8@gmail.com>
2022-08-24 21:24:15 -0400

    got errors to clear when closing modals

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,6 +1,7 @@
 //keeps track of any error messages
 
 import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from "../actions/session_actions";
+import { OPEN_MODAL, CLOSE_MODAL } from "../actions/modal_actions";
 
 //listens for 2 action types
 export default (state = {}, action) => {
@@ -10,6 +11,10 @@ export default (state = {}, action) => {
             return []; // clears the errors
         case RECEIVE_ERRORS:
             return action.errors; //sets errors to the action's errors
+        case OPEN_MODAL:
+            return [];
+        case CLOSE_MODAL:
+            return [];
         default:
             return state;
     }
