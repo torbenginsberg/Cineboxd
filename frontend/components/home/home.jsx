f43755dc8c36b3901fda026eb5c7d400b6a187e7
Torben Ginsberg <tginsberg8@gmail.com>
2022-08-25 17:11:02 -0400

    added poster links to home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import HomeBackdrop from '../../../app/assets/images/godfather-brando-1.png'
+import HomeBackdrop from '../../../app/assets/images/godfather-brando-1.png';
+import Sicario from '../../../app/assets/images/Sicario.jpeg';
+import Dunkirk from '../../../app/assets/images/dunkirk_poster.png';
+import FrancesHa from '../../../app/assets/images/francesha_poster.png';
+import Mononoke from '../../../app/assets/images/mononoke_poster.png';
+import Social from '../../../app/assets/images/social_poster.png';
+import Whiplash from '../../../app/assets/images/whiplash_poster.png';
 
 export default ({ currentUser, openModal }) => {
     return (
@@ -16,10 +22,16 @@ export default ({ currentUser, openModal }) => {
                 </div>
             </div>
             
-            <div className="home-movie-list">
-
-                
+            <div className="home-film-list">
+                <Link to="/films/4" className="poster-link"><img src={Sicario} alt="Sicario" /></Link>
+                <Link to="/films/6" className="poster-link"><img src={Mononoke} alt="Princess Mononoke" /></Link>
+                <Link to="/films/8" className="poster-link"><img src={FrancesHa} alt="Frances Ha" /></Link>
+                <Link to="/films/9" className="poster-link"><img src={Whiplash} alt="Whiplash" /></Link>
+                <Link to="/films/11" className="poster-link"><img src={Dunkirk} alt="Dunkirk" /></Link>
+                <Link to="/films/14" className="poster-link"><img src={Social} alt="The Social Network" /></Link>
             </div>
+
+            <p className=""></p>
         </div>
     )
 }
