7a7c1f42778fcebe33faa8c9ad019b9d01cb448c
Torben Ginsberg <tginsberg8@gmail.com>
2022-09-28 15:11:49 -0400

    add initial setup for logged films page

--- frontend/components/nav_header/profile_dropdown.jsx
+++ frontend/components/nav_header/profile_dropdown.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default ({ logout }) => {
+export default ({ logout, currentUserId }) => {
     const navDropdownStyle = {
         display: 'none'
     }
--- frontend/components/nav_header/profile_dropdown_container.js
+++ frontend/components/nav_header/profile_dropdown_container.js
@@ -4,11 +4,17 @@ import React from "react";
 import { logout } from "../../actions/session_actions";
 import ProfileDropdown from "./profile_dropdown";
 
+const mSTP = state => {
+    return {
+        currentUserId: state.session.id
+    }
+}
+
 const mDTP = dispatch => ({
     logout: () => dispatch(logout())
 });
 
 export default withRouter(connect(
-    null,
+    mSTP,
     mDTP
 )(ProfileDropdown));
