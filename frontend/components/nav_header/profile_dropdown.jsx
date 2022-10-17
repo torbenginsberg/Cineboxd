af38f36a2e8fd31603a5c1a4ac9bbeb642df2ccd
Torben Ginsberg <tginsberg8@gmail.com>
2022-10-17 13:36:57 -0400

    Remove profile dropdown temporarily

--- frontend/components/nav_header/profile_dropdown.jsx
+++ frontend/components/nav_header/profile_dropdown.jsx
@@ -6,14 +6,15 @@ export default ({ logout, currentUserId }) => {
         display: 'none'
     }
     return(
-        <ul className="nav-dropdown" id='profile-dropdown' style={navDropdownStyle}>
-            <li className="divider"><a href="/">Home</a></li>
-            {/* <li><a href="/">Profile</a></li> */}
-            <li><a href="/#/films">Films</a></li>
-            {/* <li><a href="/">Reviews</a></li> */}
-            {/* <li><a href="/">Watchlist</a></li>
-            <li><a href="/">Lists</a></li> */}
-            <li className="divider"><a onClick={logout}>Sign Out</a></li>
-        </ul>
+        // <ul className="nav-dropdown" id='profile-dropdown' style={navDropdownStyle}>
+        //     <li className="divider"><a href="/">Home</a></li>
+        //     {/* <li><a href="/">Profile</a></li> */}
+        //     <li><a href="/#/films">Films</a></li>
+        //     {/* <li><a href="/">Reviews</a></li> */}
+        //     {/* <li><a href="/">Watchlist</a></li>
+        //     <li><a href="/">Lists</a></li> */}
+        //     <li className="divider"><a onClick={logout}>Sign Out</a></li>
+        // </ul>
+        <ul></ul>
     );
 };
