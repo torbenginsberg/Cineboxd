15b750bcaea5b71944073013798bcad29c25bc56
Torben Ginsberg <tginsberg8@gmail.com>
2022-11-14 10:47:13 -0500

    Wrote footer component

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AiFillGithub, AiFillLinkedin, AiOutlineUser } from "react-icons/ai";
+import { FaAngellist } from "react-icons/fa";
 
 export default () => {
     return (
@@ -12,8 +14,17 @@ export default () => {
                         <li><a href="/contact">Contact</a></li>
                     </ul>
                 </nav>
-                <div className="footer-links"></div>
-                <p className="footer-message"></p>
+                <div className="footer-links">
+                    <nav className="footer-links-nav">
+                        <div className="links-list">
+                            <a href="https://www.linkedin.com/in/torben-ginsberg-095a77112/"><i><AiFillLinkedin/></i></a>
+                            <a href="https://github.com/torbenginsberg/Cineboxd"><i><AiFillGithub/></i></a>
+                            <a href="https://angel.co/u/torben-ginsberg"><i><FaAngellist/></i></a>
+                            <a href="https://torbenginsberg.github.io/"><i><AiOutlineUser/></i></a>
+                        </div>
+                    </nav>
+                </div>
+                <p className="footer-message">Cineboxd. Made by a lover of films and an avid user of Letterboxd in Brooklyn, NY.</p>
             </div>
         </footer>
     )
