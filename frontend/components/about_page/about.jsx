98da2299bb4141e1e506da8fe64b384af161e772
Torben Ginsberg <tginsberg8@gmail.com>
2022-11-28 15:27:57 -0600

    Create app component and style

---
+++ frontend/components/about_page/about.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link} from "react-router-dom";
+
+export default () => {
+    <div>
+        <div className="about-backdrop">
+            <div className="backdrop-fade">
+                <h2 className="about-title">About</h2>
+                <div className="about-text">
+                    <p>Cineboxd is a full stack clone of Letterboxd etc.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+}
